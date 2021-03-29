a9d945fbd6e27fcb49b8cac8e2758f37798f3acf
Tim Fournet <tfournet@tfour.net>
2021-03-29 17:24:36 -0500

    fix typo

--- AzureAD/Create-CyberCNSADAppRegistration.sh
+++ AzureAD/Create-CyberCNSADAppRegistration.sh
@@ -4,7 +4,7 @@
 # This is a bash script, it can be run either from a Linux host, or from the Azure Cloud Shell.
 # To run from the cloud shell, use this command
 # curl https://raw.githubusercontent.com/CyberCNS-Community/Deployment-Scripts/main/AzureAD/Create-CyberCNSADAppRegistration.sh | sh
-# This will create an Azure AD App Registration with the permissions required, as outlined by the CyberCNS Documentatino at 
+# This will create an Azure AD App Registration with the permissions required, as outlined by the CyberCNS Documentation at 
 # https://cybercns.atlassian.net/wiki/spaces/CYB/pages/1079017579/Azure+AD+Audit
 
 AppAccountName="RADER_CNS_AzureAD_Audit"
--- AzureAD/README.md
+++ AzureAD/README.md
@@ -8,5 +8,7 @@ To run from the cloud shell, use this command:
 `curl https://raw.githubusercontent.com/CyberCNS-Community/Deployment-Scripts/main/AzureAD/Create-CyberCNSADAppRegistration.sh | sh`
 
 
-This will create an Azure AD App Registration with the permissions required, as outlined by the CyberCNS Documentatino at 
+This will create an Azure AD App Registration with the permissions required, as outlined by the CyberCNS Documentation at 
 https://cybercns.atlassian.net/wiki/spaces/CYB/pages/1079017579/Azure+AD+Audit
+
+The script will output the information to plug into the CyberCNS UI.
