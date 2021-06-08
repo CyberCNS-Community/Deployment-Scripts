895631c66f9f19e0899356f2509756db0f35701f
Tim Fournet <tfournet@radersolutions.com>
2021-06-08 16:10:57 -0500

    add epel-release for dnf

--- Docker/get-docker-container.sh
+++ Docker/get-docker-container.sh
@@ -19,7 +19,7 @@ cybercns_site_id=$2
 
 if ! (which docker-compose); then
   echo "Docker Compose was not found on your system. Attempting to install it using standard distro utils"
-  which dnf && dnf -y install podman-compose curl
+  which dnf && dnf -y install dnf -y install epel-release && dnf -y install podman-compose curl
   which yum && yum -y install docker docker-compose curl 
   which apt && apt-get -y install docker docker-compose curl
   if ! (which docker-compose); then 
